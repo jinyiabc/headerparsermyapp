f52f3090f48a613ceeeb370e390d49c78af2cba1
Glitch (headerparsermyapp) <none>
2018-01-15 15:14:42 +0000

    🎐🐉 Updated with Glitch

--- README.md
+++ README.md
@@ -1,15 +1,19 @@
 Header Parser microservice
 =========================
 
-Click `Show` in the header to see your app live. Updates to your code will instantly deploy and update live.
 
-**Note** Here's the specific user story you should implement for this project:
 
 
 **User Story:** I can get the IP address, language and operating system for my browser.
 
+example: 
+========
+
+**url:** https://headerparsermyapp.glitch.me
+
+**output:** {"ipaddress":"180.188.197.10","language":"en","software":"Macintosh; Intel Mac OS X 10_13_2"}
 
 
-Made by [Alex J.Y.](jinyiabc@gmail.com)
+Made by Alex J.Y.
 -------------------
 
--- server.js
+++ server.js
@@ -13,25 +13,50 @@ app.use(express.static('public'));
 
 // http://expressjs.com/en/starter/basic-routing.html
 app.get("/", function (request, response) {
-  response.sendFile(__dirname + '/views/index.html');
-});
+  // console.log(JSON.stringify(request.headers["user-agent"]));
+  //"Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_2) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/63.0.3239.132 Safari/537.36"
+  // console.log(JSON.stringify(request.headers["accept-language"]));
+  // console.log(JSON.stringify(request.headers["x-forwarded-for"]));
+  //"180.188.197.10,::ffff:10.10.10.171,::ffff:10.10.10.47"
+  var ipaddress = request.headers["x-forwarded-for"]
+  var numberIp = ipaddress.indexOf(',')
+  var software = request.headers["user-agent"]
+  var start = software.indexOf('(')
+  var stop = software.indexOf(')')
+ 
+  // response.sendFile(__dirname + '/views/index.html');
+   var result = {
+                ipaddress:ipaddress.slice(0,numberIp),
+                language:request.headers["accept-language"],
+                software:software.slice(start+1,stop)
+              };
+   
+        if (result) {
+        response.writeHead(200, { 'Content-Type': 'application/json' })
+        response.end(JSON.stringify(result))
+      } else {
+        response.writeHead(404)
+        response.end()
+      }
+    })
 
-app.get("/dreams", function (request, response) {
-  response.send(dreams);
-});
 
-// could also use the POST body instead of query string: http://expressjs.com/en/api.html#req.body
-app.post("/dreams", function (request, response) {
-  dreams.push(request.query.dream);
-  response.sendStatus(200);
-});
+
+// app.get("/dreams", function (request, response) {
+//   response.send(dreams);
+// });
+
+// // could also use the POST body instead of query string: http://expressjs.com/en/api.html#req.bopp.post("/dreams", function (request, response) {
+//   dreams.push(request.query.dream);
+//   response.sendStatus(200);
+// });
 
 // Simple in-memory store for now
-var dreams = [
-  "Find and count some sheep",
-  "Climb a really tall mountain",
-  "Wash the dishes"
-];
+// var dreams = [
+//   "Find and count some sheep",
+//   "Climb a really tall mountain",
+//   "Wash the dishes"
+// ];
 
 // listen for requests :)
 var listener = app.listen(process.env.PORT, function () {
